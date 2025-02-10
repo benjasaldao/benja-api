f8003b53cf09d882624b4f6f7e99e76fcf39ef89
Benja saldaño <saldaobenjamin@gmail.com>
2025-02-10 16:44:12 -0300

    Added mailsending funtionality

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const routerApi = require("./routes");
+
+const { config } = require("./config/config");
+
+const MailService = require("./services/mail.service");
+const mailService = new MailService();
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require("./middlewares/errorHandler");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const allowedOrigin = config.allowedOrigin;
+console.log(allowedOrigin)
+const options = {
+  origin: (origin, callback) => {
+    if (origin == allowedOrigin) {
+      callback(null, true);
+    } else {
+      mailService.sendMail("Cors error", "api@gmail.com", `hubo un error de cors man, porque ${origin} es distinto de ${allowedOrigin}`)
+      callback(new Error("Acceso denegado"));
+    }
+  },
+};
+app.use(cors(options));
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log("App running, listening on port " + port);
+});
---
+++ services/mail.service.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+const { config } = require("./../config/config");
+
+class MailService {
+  async sendMail(name, email, message) {
+    const mail = {
+      from: config.smtpEmail,
+      to: config.recieverEmail,
+      subject: `${name} en BenjaSaldao.com!`,
+      html: `Nombre: ${name} <br/> Email: ${email} <br/> Mensaje: <br/> ${message}`,
+    };
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      secure: true,
+      port: 465,
+      auth: {
+        user: config.smtpEmail,
+        pass: config.smtpPassword,
+      },
+    });
+    const rta = await transporter.sendMail(mail);
+    return rta;
+  }
+}
+
+module.exports = MailService;
